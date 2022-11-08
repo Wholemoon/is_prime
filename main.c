1109909078c301bbc629112b92e79e244cdda091
Wholemoon <117737784+Wholemoon@users.noreply.github.com>
2022-11-08 18:01:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,32 @@
-include<stdio.h>
+#include<stdio.h>
+#include<string.h>
 
-int main(){
-  printf("prime pls work");
+char *is_prime(unsigned long long int number){
+  unsigned long long int ref = 2;
+  if(ref > number){
+    return("False");
+  }
+  while(ref*ref <= number){
+    if(number%ref == 0){
+      return("False");
+    }
+    ref++;
+  }
+  return("True");
+}
+
+void main(){
+  unsigned long long int number;
+  
+  printf("%s", "Give me a number to check: ");
+  scanf("%llu", &number);
+  
+  char *output = is_prime(number);
+  
+  if(strcmp(output, "True") == 0){
+    printf("%llu is a prime number.\n", number);
+  }
+  if(strcmp(output, "False") == 0){
+    printf("%llu is not a prime number.\n", number);
+  }
 }
