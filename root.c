df83aa171d3f45bca4246c378a52bd20e8d3caa4
Wholemoon <117737784+Wholemoon@users.noreply.github.com>
2022-11-14 21:04:04 +0100

    Create root.c

---
+++ root.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+double root(double number){
+  double numb=number, ref=2;
+  int c=0;
+  numb=number/ref;
+  while(c<100){
+    c++;
+    ref=(numb+ref)/2;
+    numb=number/ref;
+  }
+  return(numb);
+}
+
+int main(){
+  double number;
+  printf("%s", "Give me a number and I will give you the root: ");
+  scanf("%lf", &number);
+  printf("%.10lf\n", root(number));
+  return(0);
+}
